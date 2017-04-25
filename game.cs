a1fdd348e12a0a945d0efc764a148f57958bb255
DvanLaar <d.vanlaar@students.uu.nl>
2017-04-25 11:28:37 +0200

    Exercise 1

--- game.cs
+++ game.cs
@@ -17,6 +17,16 @@ class Game
 		screen.Clear( 0 );
 		screen.Print( "hello world", 2, 2, 0xffffff );
         screen.Line(2, 20, 160, 20, 0xff0000);
+        int dx = screen.width / 2 - 128;
+        int dy = screen.height / 2 - 128;
+        for (int x = 0; x < 256; x++)
+        {
+            for (int y = 0; y < 256; y++)
+            {
+                int location = (x + dx) + (y + dy) * screen.width;
+                screen.pixels[location] = x;
+            }
+        }
 	}
 }
 
