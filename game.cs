87b92702e75e7a56d0252f65313adea4da5b483d
Jasper Everink <jeverink@hotmail.nl>
2017-05-04 11:44:42 +0200

    Exercise 5 (Function plotter)

--- game.cs
+++ game.cs
@@ -1,60 +1,137 @@
-﻿using System;
+﻿using OpenTK.Input;
+using System;
 using System.IO;
 
 namespace Template {
 
-class Game
-{
-	// member variables
-	public Surface screen;
-    public float a;
+    class Game
+    {
+        // member variables
+        public Surface screen;
 
-    public float minX = -5, maxX = 2, minY = -5, maxY = 2;
+        public float minX = -1, maxX = 1, minY = -1, maxY = 1;
+        public float zoomSpeed = 0.1f;
+        public float translateXSpeed = 0.1f;
+        public float translateYSpeed = 0.1f;
 
-	// initialize
-	public void Init()
-	{
-        a = 0;
-	}
-	// tick: renders one frame
-	public void Tick()
-	{
-        screen.Clear(0);
-        a += 0.1f;
-        float[] x = new float[] {-1, 1, 1, -1};
-        float[] y = new float[] { 1, 1, -1, -1 };
-        float[] rx = new float[4];
-        float[] ry = new float[4];
-
-        for (int i = 0; i < 4; i++)
+        // initialize
+        public void Init()
         {
-            rx[i] = (float)(x[i] * Math.Cos(a) - y[i] * Math.Sin(a));
-            ry[i] = (float)(x[i] * Math.Sin(a) + y[i] * Math.Cos(a));
         }
 
-        screen.Line(TX(rx[0]), TY(ry[0]), TX(rx[1]), TY(ry[1]), 0xffffff);
-        screen.Line(TX(rx[1]), TY(ry[1]), TX(rx[2]), TY(ry[2]), 0xffffff);
-        screen.Line(TX(rx[2]), TY(ry[2]), TX(rx[3]), TY(ry[3]), 0xffffff);
-        screen.Line(TX(rx[3]), TY(ry[3]), TX(rx[0]), TY(ry[0]), 0xffffff);
-	}
+        // tick: renders one frame
+        public void Tick()
+        {
+            screen.Clear(0);
 
-    /// <summary>
-    /// Transforms X-coordinate from world coordinates to screen coordinates
-    /// </summary>
-    private int TX(float x)
-    {
-        float worldWidth = maxX - minX;
-        return (int)((x - minX)*(screen.width/worldWidth));
+            KeyboardInput();
+
+            DrawGridLines(0x008800, 0x004400, 1f, 1f);
+            PlotFunction(0.1f, 0xff0000);
+            PlotPolarFunction(0.01f, 0x0000ff,0,(float)Math.PI*10f);
+            screen.Print("x*sin(x)",5,5, 0xff0000);
+            screen.Print("t*sin(t)*cos(t)", 5, 25, 0x0000ff);
+        }
+
+        private void DrawLine(float x1, float y1, float x2, float y2, int color)
+        {
+            screen.Line(TX(x1), TY(y1), TX(x2), TY(y2), color);
+        }
+
+        private void KeyboardInput()
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+            //Zoom out
+            if (keyboard[Key.Z]) { minX -= zoomSpeed; maxX += zoomSpeed; minY -= zoomSpeed; maxY += zoomSpeed; }
+            //Zoom in
+            if (keyboard[Key.X]
+                && minX + zoomSpeed < maxX - zoomSpeed
+                && minY + zoomSpeed < maxY - zoomSpeed) { minX += zoomSpeed; maxX -= zoomSpeed; minY += zoomSpeed; maxY -= zoomSpeed; }
+            //Move left
+            if (keyboard[Key.Left]) { minX -= translateXSpeed; maxX -= translateXSpeed; }
+            //Move right
+            if (keyboard[Key.Right]) { minX += translateXSpeed; maxX += translateXSpeed; }
+            //Move up
+            if (keyboard[Key.Up]) { minY += translateYSpeed; maxY += translateYSpeed; }
+            //Move down
+            if (keyboard[Key.Down]) { minY -= translateYSpeed; maxY -= translateYSpeed; }
+
+        }
+
+        private void DrawGridLines(int coloraxis, int colorlines, float horizontalstepsize, float verticalstepsize)
+        {
+            //Horizontal
+            for (float y = (float)Math.Floor(minY); y < maxY + verticalstepsize; y += verticalstepsize)
+            {
+                DrawLine(minX, y, maxX, y, colorlines);
+                DrawLine(minX, y - (verticalstepsize / 2f), maxX, y - (verticalstepsize / 2), colorlines / 4);
+            }
+            //Vertical
+            for (float x = (float)Math.Floor(minX); x < maxX + horizontalstepsize; x += horizontalstepsize)
+            {
+                DrawLine(x, minY, x, maxY, colorlines);
+                DrawLine(x - (horizontalstepsize / 2f), minY, x - (horizontalstepsize / 2f), maxY, colorlines / 4);
+            }
+            //X-Axis
+            DrawLine(minX, 0, maxX, 0, coloraxis);
+            //Y-Axis
+            DrawLine(0, minY, 0, maxY, coloraxis);
+        }
+
+        private void PlotFunction(float stepSize, int color)
+        {
+            if (stepSize <= 0f) //To avoid infinite loop
+                return;
+            float prevValue = evaluateFunction(minX);
+            for (float x = minX + stepSize; x <= maxX; x += stepSize)
+            {
+                float value = evaluateFunction(x);
+                DrawLine(x - stepSize, prevValue, x, value, color);
+                prevValue = value;
+            }
+        }
+
+        private void PlotPolarFunction(float stepSize, int color, float minTheta = (float)-Math.PI * 2f, float maxTheta = (float)Math.PI * 2f)
+        {
+            if (stepSize <= 0f) //To avoid infinite loop
+                return;
+            float prevValue = evaluatePolarFunction(minTheta);
+            for (float theta = minTheta + stepSize; theta <= maxTheta; theta += stepSize)
+            {
+                float value = evaluatePolarFunction(theta);
+                DrawLine(prevValue * (float)Math.Cos(theta - stepSize), prevValue * (float)Math.Sin(theta - stepSize), value * (float)Math.Cos(theta), value * (float)Math.Sin(theta), color);
+                prevValue = value;
+            }
+        }
+
+        /// <summary>
+        /// Transforms X-coordinate from world coordinates to screen coordinates
+        /// </summary>
+        private int TX(float x)
+        {
+            float worldWidth = maxX - minX;
+            return (int)((x - minX) * (screen.width / worldWidth));
+        }
+
+        /// <summary>
+        /// Transforms Y-coordinate from world coordinates to screen coordinates
+        /// </summary>
+        private int TY(float y)
+        {
+            float worldHeight = maxY - minY;
+            return (int)((-y + maxY) * (screen.height / worldHeight));
+        }
+
+
+        private float evaluateFunction(float x)
+        {
+            return (float)(Math.Sin(x) * x);
+        }
+
+        private float evaluatePolarFunction(float theta)
+        {
+            return (float)(theta*Math.Cos(theta)*Math.Sin(theta));
+        }
+
     }
-
-    /// <summary>
-    /// Transforms Y-coordinate from world coordinates to screen coordinates
-    /// </summary>
-    private int TY(float y)
-    {
-        float worldHeight = maxY - minY;
-        return (int)((-y+maxY)*(screen.height/worldHeight));
-    }
-}
-
 } // namespace Template
