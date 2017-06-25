f0a9b6d14f3228f02cb6a8cf6dd2e17a1b5fe535
Jasper Everink <jeverink@hotmail.nl>
2017-06-25 16:41:48 +0200

    Reflective is now actually reflecting

--- Assignment 3/Engine/shaders/fs_reflective.glsl
+++ Assignment 3/Engine/shaders/fs_reflective.glsl
@@ -5,6 +5,8 @@ in vec2 uv; // interpolated texture coordinates
 in vec4 normal; // interpolated normal
 in vec3 position;
 
+uniform vec3 camerapos;
+
 // shader output
 layout(location = 0) out vec4 outputColor;
 layout(location = 1) out vec4 outputHDR;
@@ -15,6 +17,8 @@ uniform samplerCube skybox;
 // fragment shader
 void main()
 {
-    outputColor = vec4(materialcolor, 1) * texture(skybox, normal.xyz);
+	vec3 refl = reflect(normalize(position-camerapos),normalize(normal.xyz));
+
+    outputColor = vec4(materialcolor, 1) * texture(skybox, refl);
     outputHDR = clamp(outputColor - vec4(1f, 1f, 1f, 1f), 0, 1);
 }
